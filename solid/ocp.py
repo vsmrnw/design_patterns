d75aeb154c7cdb550a6c803c5dc607dc89c0aa67
vsmrnw <vsevolod.smrnv@gmail.com>
2024-04-15 09:41:37 +0300

    Initial commit

---
+++ solid/ocp.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+class Size(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+
+class Product:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.color = color
+        self.size = size
+
+class ProductFilter:
+    def filter_by_color(self, products, color):
+        for p in products:
+            if p.color == color: yield p
+
+    def filter_by_size(self, products, size):
+        for p in products:
+            if p.size == size: yield p
---
+++ solid/srp.py
@@ -0,0 +1,46 @@
+# SRP SOC
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.count += 1
+        self.entries.append(f"{self.count}: {text}")
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return "\n".join(self.entries)
+    # break SRP
+    # def save(self, filename):
+    #     file = open(filename, 'w')
+    #     file.write(str(self))
+    #     file.close()
+    #
+    # def load(self, filename):
+    #     pass
+    #
+    # def load_from_web(self, uri):
+    #     pass
+
+
+class PersistenceManager:
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
+
+
+j = Journal()
+j.add_entry('I went to the work')
+j.add_entry('I cried today')
+print(f"Journal entries:\n{j}")
+
+file = r'/tmp/journal.txt'
+PersistenceManager.save_to_file(j, file)
+
+with open(file) as f:
+    print(f.read())
